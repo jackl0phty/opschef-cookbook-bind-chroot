03869db64c2ad590d41674538fcd9fb56307c940
Gerald L. Hevener Jr., M.S <jackl0phty@gmail.com>
2013-05-28 21:14:37 -0400

    Edit metadata.rb.

--- templates/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Gerald L. Hevener Jr., M.S."
-maintainer_email "jackl0phty@gmail.com"
-license          ""
-description      "Installs/Configures bind-chroot"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.10"
